69af1355d9d75b5056c000513c79da239107120c
juhyeon <juhyeon1114@gmail.com>
2024-11-19 20:25:50 +0900

    init

---
+++ src/main/java/demo/ecorichhomework/EcorichhomeworkApplication.java
@@ -0,0 +1,13 @@
+package demo.ecorichhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcorichhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcorichhomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/ecorichhomework/EcorichhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ecorichhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcorichhomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
